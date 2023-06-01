33410fa5d30926a07cae5b5b8fd2fa7ce47d378b
pradeep ramidi <pradeep.ramidi@rmit.edu.au>
2023-06-02 07:07:58 +1000

    initial

---
+++ cfn-macro.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CfnMacro from '../lib/cfn-macro-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cfn-macro-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CfnMacro.CfnMacroStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
